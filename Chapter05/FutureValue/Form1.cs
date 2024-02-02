3dcc7ed4ef8dd2c07ffb0bafe3e20cfca224dfde
heayem <heayem7@gmail.com>
2024-02-02 18:47:07 +0700

    fix: change logic code in future value on chapter 05

--- Chapter05/FutureValue/Form1.cs
+++ Chapter05/FutureValue/Form1.cs
@@ -16,39 +16,24 @@ namespace FutureValue
         {
             InitializeComponent();
         }
-
-        private decimal GetFutureValue(decimal monthlyInvestment, decimal monthlyInterestRate = 0.05m, int months = 12)
+        private void btnCalculate_Click(object sender, EventArgs e)
         {
+            decimal monthlyInvestment = Convert.ToDecimal(txtMonthlyInvestment.Text);
+            decimal yearlyInterestRate = Convert.ToDecimal(txtYearlyInterestRate.Text);
+            int years = Convert.ToInt32(txtNumberOfYears.Text);
+
+            int months = years * 12;
+            decimal monthlyInterestRate
+                = yearlyInterestRate / 12 / 100;
+
             decimal futureValue = 0m;
             for (int i = 0; i < months; i++)
             {
-                futureValue = (futureValue + monthlyInvestment) *
-                              (1 + monthlyInterestRate);
+                futureValue = (futureValue + monthlyInvestment)
+                            * (1 + monthlyInterestRate);
             }
-            return futureValue;
-        }
-
-        private void btnCalculate_Click(object sender, EventArgs e)
-        {
-                decimal monthlyInvestment
-                    = Convert.ToDecimal(txtMonthlyInvestment.Text);
-                decimal yearlyInterestRate
-                    = Convert.ToDecimal(txtYearlyInterestRate.Text);
-                int years = Convert.ToInt32(txtNumberOfYears.Text);
-
-                int months = years * 12;
-                decimal monthlyInterestRate
-                    = yearlyInterestRate / 12 / 100;
-
-                decimal futureValue = 0m;
-                for (int i = 0; i < months; i++)
-                {
-                    futureValue = (futureValue + monthlyInvestment)
-                                * (1 + monthlyInterestRate);
-                }
-
-                txtFutureValue.Text = futureValue.ToString("c");
-                txtMonthlyInvestment.Focus();
+            txtFutureValue.Text = futureValue.ToString("c");
+            txtMonthlyInvestment.Focus();
         }
 
         private void btnExit_Click(object sender, EventArgs e)
